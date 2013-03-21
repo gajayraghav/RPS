a10782532e89f55809bec3d76131a6a11c514758
Ajay Raghav <gajayraghav@gmail.com>
2013-03-21 02:12:56 -0400

    Unified Starting Screen

--- RPS1/src/com/netowrks/rps1/MainActivity.java
+++ RPS1/src/com/netowrks/rps1/MainActivity.java
@@ -2,14 +2,24 @@ package com.netowrks.rps1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		findViewById(R.id.bBasicChat1).setOnClickListener(this);
+		findViewById(R.id.bChat1).setOnClickListener(this);
+		findViewById(R.id.bRegistration1).setOnClickListener(this);
+		findViewById(R.id.bHome1).setOnClickListener(this);
+		findViewById(R.id.bMap1).setOnClickListener(this);
+		findViewById(R.id.bNavigate1).setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +27,43 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent nextActivity;
+		switch(v.getId())
+		{
+			case R.id.bBasicChat1:
+				nextActivity = new Intent(this, BasicChat.class);
+				this.startActivity(nextActivity);
+				break;
+			case R.id.bChat1:
+				nextActivity = new Intent(this, Chat.class);
+				this.startActivity(nextActivity);
+				break;
+			
+			case R.id.bMap1:
+				nextActivity = new Intent(this, Map.class);
+				this.startActivity(nextActivity);
+				break;
+				
+			case R.id.bNavigate1:
+				nextActivity = new Intent(this, Navigate.class);
+				this.startActivity(nextActivity);
+				break;
+				
+			case R.id.bRegistration1:
+				nextActivity = new Intent(this, Registration.class);
+				this.startActivity(nextActivity);
+				break;
+				
+			case R.id.bHome1:
+				nextActivity = new Intent(this, Home.class);
+				this.startActivity(nextActivity);
+				break;
+		}
 	}
 
 }
